1789260c7180608f87caedb81991d7e3ed0881da
John Klein <zarkon@law.harvard.edu>
2016-05-06 11:48:12 -0400

    Hide grades on AJAX calls

--- js/hide-grades.js
+++ js/hide-grades.js
@@ -1,3 +1,10 @@
-$(document).ready(function() {
+function hlsHideGrades() {
     $('.Button.button-sidebar-wide:contains(View Grades)').hide();
+}
+$(document).ready(function() {
+    hlsHideGrades();
 });
+$(document).ajaxComplete(function() {
+    hlsHideGrades();
+});
+
